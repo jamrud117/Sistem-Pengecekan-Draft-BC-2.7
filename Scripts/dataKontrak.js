9c77afbd62978734126cf781e950e06bd7ea0216
jamrud117 <yogi.insider10@gmail.com>
2025-10-01 08:48:06 +0700

    Ganti detail kontrak

--- Scripts/dataKontrak.js
+++ Scripts/dataKontrak.js
@@ -1,13 +1,13 @@
 const kontrakData = [
   {
     pt: "PT. Bintang Indokarya Gemilang",
-    no: "CPMI/01/IX/2025-01",
-    tgl: "2025-09-01",
+    no: "CPMI/01/X/2025-01",
+    tgl: "2025-10-01",
   },
   {
     pt: "PT. Shoetown Ligung Indonesia",
-    no: "CPMI/01/IX/2025-01",
-    tgl: "2025-09-01",
+    no: "CPMI/01/X/2025-01",
+    tgl: "2025-10-01",
   },
   {
     pt: "PT. Long Rich Indonesia",
